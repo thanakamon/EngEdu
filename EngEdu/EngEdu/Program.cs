1c0237901442d849f0e6730212049ffbd7831091
thanakamon <thanakamon>
2018-08-24 20:59:34 +0700

    2nd  year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -12,6 +12,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 255x semester 1, I am an Engedu freshman");
             Console.WriteLine("Year 255x semester 2,I learn to program in c shape");
+            Console.WriteLine("Year 2561 semester 1,I have to regrade on some course");
         }
     }
 }
