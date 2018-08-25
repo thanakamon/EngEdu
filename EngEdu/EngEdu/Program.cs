259818f01e64c432655f920c24e38789a243d7bb
thanakamon <thanakamon>
2018-08-25 17:32:52 +0700

    4th year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -16,6 +16,7 @@ namespace EngEdu
             Console.WriteLine("Year 2561 semester 2,I am good");
             Console.WriteLine("Year 2562 semester 1,I am okay");
             Console.WriteLine("Year 2562 semester 2,I learn to program everyday");
+            Console.WriteLine("Year 2563 semester 1,I have a project");
         }
     }
 }
