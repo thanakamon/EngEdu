f51ed8e95df9ca7446bf89eb6b69760b2e00287e
thanakamon <thanakamon>
2018-08-25 18:10:16 +0700

    3rd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 2,I learn to program in c sharp");
             Console.WriteLine("Year 2561 semester 1,I have to regrade on some course");
             Console.WriteLine("Year 2561 semester 2,I am an Engedu sophomore");
+            Console.WriteLine("Year 2562 semester 1,I am an Engedu junior ");
         }
     }
 }
